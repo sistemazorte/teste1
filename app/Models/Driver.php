c3825061a3bd8134252f5ba8cc6c1f7a85f6ca91
Gustavo Miranda <gustavosperfeldmiranda@gmail.com>
2026-03-07 13:43:38 -0300

    feat: configura campos permitidos e conversao boolean no model Driver

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Driver extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'cpf',
+        'cnh_category',
+        'phone',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
 }
