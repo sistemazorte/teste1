4204fc1c9583d227a5e75363e104c938c4405073
Gustavo Miranda <gustavosperfeldmiranda@gmail.com>
2026-03-08 16:46:24 -0300

    fix: remove os estilos de animação de fade-in que não são utilizados do componente DriversPage.

--- resources/js/pages/drivers/index.tsx
+++ resources/js/pages/drivers/index.tsx
@@ -785,22 +785,6 @@ export default function DriversPage() {
                     </div>
                 </section>
             </div>
-
-            <style>{`
-                @keyframes fade-in {
-                    from {
-                        opacity: 0;
-                        transform: scale(0.95);
-                    }
-                    to {
-                        opacity: 1;
-                        transform: scale(1);
-                    }
-                }
-                .animate-fade-in {
-                    animation: fade-in 0.2s ease-out;
-                }
-            `}</style>
         </div>
     );
 }
